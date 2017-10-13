0c6ea04ac3755f7a4abc134d6caefd60159582a2
esthete88 <esthete88@gmail.com>
2017-10-13 17:46:06 +0300

    first task done

--- 3 season/mycron/mycron.cpp
+++ 3 season/mycron/mycron.cpp
@@ -5,13 +5,58 @@
 #include <vector>
 #include <fstream>
 #include <iostream>
+#include <cmath>
+#include <ctime>
+#include <sys/stat.h>
 
 using namespace std;
 
 struct Task {
-        int hh, mm, ss;
+        int h, m, s;
         string line;
 };
+
+
+int checkSymbol(char);
+vector<string> parseStr(string S, char delimiter);
+string* splitTask(string S);
+int strToInt(string S);
+Task parseToTask(string S);
+void launch(string S);
+
+
+int main() {
+        ifstream file("mycrontab.txt");
+        vector<Task> tasks;
+        string temp;
+        struct stat buf;
+        stat("mycrontab.txt", &buf);
+        time_t raw_time, mtime = buf.st_mtime;
+        tm* times;
+        getline(file, temp);
+        while (temp != "") {
+                auto ptt = parseToTask(temp);
+                if (ptt.h >= -1 && ptt.h < 24)
+                        tasks.emplace_back(ptt);
+                getline(file, temp);
+        }
+        while (true) {
+                time(&raw_time);
+                times = localtime(&raw_time);
+                stat("mycrontab.txt", &buf);
+                if (mtime != buf.st_mtime)
+                        main();
+                for (auto task : tasks) {
+                        if ((task.h == times->tm_hour || task.h == -1) &&
+                            (task.m == times->tm_min || task.m == -1) &&
+                            (task.s == times->tm_sec || task.s == -1)) {
+                                launch(task.line);
+                        }
+                }
+                sleep(1);
+        }
+        return 0;
+}
 
 int checkSymbol(char c) {
         if (c >= 48 && c <= 57) { // number
@@ -23,18 +68,94 @@ int checkSymbol(char c) {
         }
 }
 
-int main(int argc, char const *argv[]) {
-        ifstream file("mycrontab.txt");
-        vector<string> vargs(3);
+vector<string> parseStr(string S, char delimiter) {
+        size_t pos;
+        vector<string> Split;
+        string temp;
+        while((pos = S.find(delimiter)) != string::npos) {
+                temp = S.substr(0, pos);
+                Split.emplace_back(temp);
+                S.erase(0, pos + 1);
+        }
+        Split.emplace_back(S);
+        return Split;
+}
+
+string* splitTask(string S) {
+        size_t pos;
+        string* split = new string [2];
+        pos = S.find(' ');
+        if (pos != string::npos) {
+                split[0] = S.substr(0, pos);
+                S.erase(0, pos + 1);
+                split[1] = S;
+        }
+        else {
+                cout << "Incorrect input" << endl;
+                return NULL;
+        }
+        return split;
+}
+
+int strToInt(string S) {
+        int x = 0, length = S.size();
+        for (int i = 0; i < length; ++i) {
+                x += (S[i]-48) * pow(10, length - i - 1);
+        }
+        return x;
+}
+
+Task parseToTask(string S) {
+        auto split = splitTask(S);
+        Task task;
+        if (split == NULL) {
+                return task;
+        }
+        task.line = split[1];
+        auto time = parseStr(split[0], ':');
+        if (time.size() != 3) {
+                cout << "Incorrect time format" << endl;
+                return task;
+        }
+        for (int i = 0; i < 3; ++i) {
+                int p = checkSymbol(time[i][0]);
+                switch (i) {
+                case 0:
+                        if (p == 0)
+                                task.h = strToInt(time[i]);
+                        else if (p == 1)
+                                task.h = -1;
+                        break;
+                case 1:
+                        if (p == 0)
+                                task.m = strToInt(time[i]);
+                        else if (p == 1)
+                                task.m = -1;
+                        break;
+                default:
+                        if (p == 0)
+                                task.s = strToInt(time[i]);
+                        else if (p == 1)
+                                task.s = -1;
+                        break;
+                }
+        }
+        return task;
+}
+
+void launch(string S) {
+        auto vargs = parseStr(S, ' ');
         vector<const char*> args;
-        vargs[0] = "ls";
-        vargs[1] = "-l";
-        vargs[2] = "/";
         for (auto const &q : vargs) {
                 args.push_back(q.c_str());
         }
         args.push_back(NULL);
-        // printf("%s\n", (char**)&args[0]);
-        execvp(args[0], (char**)&args[0]);
-        return 0;
+        pid_t pid = fork();
+        if (pid == 0) {
+                int num = execvp(args[0], (char**)&args[0]);
+                if (num == -1) {
+                        perror("Process was not executed");
+                        exit(123);
+                }
+        }
 }
